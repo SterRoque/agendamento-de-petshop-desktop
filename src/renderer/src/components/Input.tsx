ec4ef6b3496f37c4edc305ba339b849b37b99682
Sterphane Roque <stermiss2@gmail.com>
2025-02-23 15:41:03 -0300

    feat: add input time and date

--- src/renderer/src/components/Input.tsx
+++ src/renderer/src/components/Input.tsx
@@ -1,20 +1,44 @@
+import { useRef } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+
 type InputProps = {
   iconSrc?: string;
   placeholder?: string;
   label?: string;
+  type?: "text" | "date" | "time";
 };
 
-export function Input({ iconSrc, placeholder, label }: InputProps) {
+export function Input({
+  iconSrc,
+  placeholder,
+  label,
+  type = "text",
+}: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <div>
-      <p className="text-white text-sm font-bold mb-1">{label}</p>
-      <div className=" w-full flex p-2 border border-[#3E3C41] rounded-md gap-2">
-        <img src={iconSrc} className="w-[17px] h-[17px]" />
+    <div className="w-full">
+      {label && <p className="text-white text-sm font-bold mb-1">{label}</p>}
+      <div className="w-full flex p-2 border border-[#3E3C41] rounded-md gap-2 h-12 items-center">
+        {iconSrc && (
+          <img src={iconSrc} alt="icon" className={`w-[17px] h-[17px] -mt-1`} />
+        )}
         <input
-          type="text"
-          className="focus:outline-none text-sm bg-transparent text-[#98959D]"
+          type={type}
+          ref={inputRef}
+          className={`focus:outline-none text-sm bg-transparent text-[#98959D] appearance-none custom-date-input ${(type === "date" || type === "time") && "cursor-pointer"}`}
           placeholder={placeholder}
+          onClick={() => inputRef.current?.showPicker()}
         />
+
+        {(type === "date" || type === "time") && (
+          <div
+            className="w-full relative cursor-pointer"
+            onClick={() => inputRef.current?.showPicker()}
+          >
+            <IoIosArrowDown color="white" className="absolute right-2" />
+          </div>
+        )}
       </div>
     </div>
   );
