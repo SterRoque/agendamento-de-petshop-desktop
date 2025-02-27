e17dc5e7d292609d4b3ccd017f97dc7b652d3a05
Sterphane Roque <stermiss2@gmail.com>
2025-02-27 00:25:38 -0300

    feat: add height

--- src/renderer/src/components/Button.tsx
+++ src/renderer/src/components/Button.tsx
@@ -6,10 +6,10 @@ type ButtonProps = {
   isBlur?: boolean;
 };
 
-export function Button({ children, onClick, isBlur }: ButtonProps) {
+export function Button({ children, onClick, isBlur = false }: ButtonProps) {
   return (
     <button
-      className={`py-3 px-6 bg-[#9282FA] uppercase font-medium rounded-lg ${isBlur && "drop-shadow-[0_0_10px_#9282FA60]"}`}
+      className={`py-3 px-6 max-h-12 bg-[#9282FA] uppercase font-medium rounded-lg ${isBlur && "drop-shadow-[0_0_10px_#9282FA70]"}`}
       onClick={onClick}
     >
       {children}
