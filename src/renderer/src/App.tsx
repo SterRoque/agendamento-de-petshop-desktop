d31ec58bcdd8f118b520f8a61a1ec88ccedb0365
Sterphane Roque <stermiss2@gmail.com>
2025-03-01 21:07:02 -0300

    feat: add loading context, loading and home in app

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,47 +1,13 @@
-import { Input } from "./components/Input";
-import RegisterModal from "./components/RegisterModal";
-import { Card } from "./components/Card";
-import date from "./assets/icons/date.svg";
-import { Button } from "./components/Button";
-import { useState } from "react";
-import { useGetAppointments } from "./hooks/useGetAppointments";
+import Loading from "./components/Loading";
+import { LoadingContextProvider } from "./contexts/LoadingContext";
+import { Home } from "./pages/Home";
 
 function App(): JSX.Element {
-  const [isOpenRegisterModal, setIsOpenRegisterModal] =
-    useState<boolean>(false);
-
-  const { appointments } = useGetAppointments();
-
   return (
-    <div className="h-screen bg-[#14151D] w-full flex justify-center relative">
-      <div className="max-w-[711px] ">
-        <div className="flex justify-between mt-16 mb-10">
-          <div>
-            <h1 className="text-2xl text-white font-bold mb-1">Sua agenda</h1>
-            <p className="text-[#98959D] text-sm font-medium">
-              Aqui você pode ver todos os clientes e serviços agendados para
-              hoje.
-            </p>
-          </div>
-          <Input type="date" iconSrc={date} />
-        </div>
-        <div className="flex flex-col gap-3">
-          <Card period="morning" appointments={appointments.morning} />
-          <Card period="afternoon" appointments={appointments.afternoon} />
-          <Card period="night" appointments={appointments.night} />
-        </div>
-
-        <RegisterModal
-          isOpen={isOpenRegisterModal}
-          onClose={() => setIsOpenRegisterModal(false)}
-        />
-      </div>
-      <div className="fixed bottom-8 right-7">
-        <Button isBlur={true} onClick={() => setIsOpenRegisterModal(true)}>
-          Novo Agendamento
-        </Button>
-      </div>
-    </div>
+    <LoadingContextProvider>
+      <Loading />
+      <Home />
+    </LoadingContextProvider>
   );
 }
 
