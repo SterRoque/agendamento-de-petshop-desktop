2e20090388742ec060240607bee67b4026214f89
Sterphane Roque <stermiss2@gmail.com>
2025-03-01 21:55:37 -0300

    feat: add appointment context provider

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,15 @@
 import Loading from "./components/Loading";
+import { AppointmentContextProvider } from "./contexts/AppointmentContext";
 import { LoadingContextProvider } from "./contexts/LoadingContext";
 import { Home } from "./pages/Home";
 
 function App(): JSX.Element {
   return (
     <LoadingContextProvider>
-      <Loading />
-      <Home />
+      <AppointmentContextProvider>
+        <Loading />
+        <Home />
+      </AppointmentContextProvider>
     </LoadingContextProvider>
   );
 }
